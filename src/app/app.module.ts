fd7bbc477fa14b0d792776e9e9f014809387d2a9
AmeeraShafiqa <ameeraashafiqaa@gmail.com>
2023-04-17 17:49:43 +0800

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Login/Login.component';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AppRoutingModule } from './app-routing.module';
+import { MatCardModule } from '@angular/material/card';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+      LoginComponent
+   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    MatCardModule,
+    NgModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
